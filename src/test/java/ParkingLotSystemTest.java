adc43eb001d3701a64ad88a9ed9769ff946d9d1e
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-15 19:40:53 +0530

    ADDED : Test cases for unparking the car

--- src/test/java/ParkingLotSystemTest.java
+++ src/test/java/ParkingLotSystemTest.java
@@ -1,11 +1,32 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotSystemTest {
+    ParkingLotSystem parkingLotSystem = null;
+    Object vehicle = null;
+    @Before
+    public void setUp() throws Exception {
+        parkingLotSystem = new ParkingLotSystem();
+        vehicle = new Object();
+    }
+
     @Test
     public void givenAVehicle_WhenParked_ShouldReturnTrue() {
-        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
         boolean isParked = parkingLotSystem.park(new Object());
         Assert.assertTrue(isParked);
+    }
+
+    @Test
+    public void givenAVehicle_WhenUnParked_ShouldReturnTrue() {
+        parkingLotSystem.park(vehicle);
+        boolean isUnParked = parkingLotSystem.unPark(vehicle);
+        Assert.assertTrue(isUnParked);
+    }
+
+    @Test
+    public void givenAWrongVehicle_WhenTriedToUnPark_ShouldReturnFalse() {
+        boolean isUnParked = parkingLotSystem.unPark(new Object());
+        Assert.assertFalse(isUnParked);
     }
 }
