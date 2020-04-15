d1aff08e55a0e8ce679ef43205a33225dc20040d
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-15 19:11:18 +0530

    REFACTORED : Removed unnecessary code

--- src/main/java/ParkingLotSystem.java
+++ src/main/java/ParkingLotSystem.java
@@ -1,7 +1,4 @@
 public class ParkingLotSystem {
-    public static void main(String[] args) {
-        System.out.println("Welcome to parking lot problem");
-    }
 
     public boolean park(Object vehicle) {
         return true;
