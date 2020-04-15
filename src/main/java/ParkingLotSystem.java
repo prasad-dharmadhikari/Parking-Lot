0a45a2929138da78b51d89a7172d5ab56e7567c0
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-15 19:39:31 +0530

    ADDED : Method for unparking the car

--- src/main/java/ParkingLotSystem.java
+++ src/main/java/ParkingLotSystem.java
@@ -1,6 +1,17 @@
 public class ParkingLotSystem {
 
+    private Object vehicle;
+
     public boolean park(Object vehicle) {
+        this.vehicle = vehicle;
         return true;
+    }
+
+    public boolean unPark(Object vehicle) {
+        if (vehicle.equals(this.vehicle)) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
     }
 }
