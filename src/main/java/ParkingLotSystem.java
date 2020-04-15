f2a1105213f74fe107a4bf85ab9462514effda5e
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-15 19:09:18 +0530

    ADDED : Method to park the car

--- src/main/java/ParkingLotSystem.java
+++ src/main/java/ParkingLotSystem.java
@@ -2,4 +2,8 @@ public class ParkingLotSystem {
     public static void main(String[] args) {
         System.out.println("Welcome to parking lot problem");
     }
+
+    public boolean park(Object vehicle) {
+        return true;
+    }
 }
