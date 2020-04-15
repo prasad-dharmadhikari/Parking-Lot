e636cf145d28d559fe4c617ad1d7d32e8500a9df
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-15 19:10:01 +0530

    ADDED : Test case to park the car

--- src/test/java/ParkingLotSystemTest.java
+++ src/test/java/ParkingLotSystemTest.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class ParkingLotSystemTest {
+    @Test
+    public void givenAVehicle_WhenParked_ShouldReturnTrue() {
+        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+        boolean isParked = parkingLotSystem.park(new Object());
+        Assert.assertTrue(isParked);
+    }
 }
