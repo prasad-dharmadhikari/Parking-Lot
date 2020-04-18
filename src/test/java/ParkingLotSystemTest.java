78f724a8da86275fb055cf4b432888678d1e4ee6
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-18 22:06:52 +0530

    REFACTORED : All test cases according to code

--- src/test/java/ParkingLotSystemTest.java
+++ src/test/java/ParkingLotSystemTest.java
@@ -8,21 +8,21 @@ public class ParkingLotSystemTest {
 
     @Before
     public void setUp() throws Exception {
-        parkingLotSystem = new ParkingLotSystem();
+        parkingLotSystem = new ParkingLotSystem(5);
         vehicle = new Object();
     }
 
     @Test
     public void givenAVehicle_WhenParked_ShouldReturnTrue() throws ParkingLotSystemException {
-        parkingLotSystem.park(new Object());
+        parkingLotSystem.park("Tata Indigo CS");
         boolean isVehicleParked = parkingLotSystem.isVehicleParked();
         Assert.assertTrue(isVehicleParked);
     }
 
     @Test
     public void givenAVehicle_WhenUnParked_ShouldReturnTrue() throws ParkingLotSystemException {
-        parkingLotSystem.park(vehicle);
-        parkingLotSystem.unPark(vehicle);
+        parkingLotSystem.park("Toyota Fortuner");
+        parkingLotSystem.unPark("Toyota Fortuner");
         boolean isVehicleUnParked = parkingLotSystem.isVehicleParked();
         Assert.assertFalse(isVehicleUnParked);
     }
@@ -31,18 +31,22 @@ public class ParkingLotSystemTest {
     public void givenAWrongVehicle_WhenTriedToUnPark_ShouldThrowException() {
 
         try {
-            parkingLotSystem.park(vehicle);
-            parkingLotSystem.unPark(new Object());
+            parkingLotSystem.park("Tata Indigo CS");
+            parkingLotSystem.unPark("Toyota Fortuner");
         } catch (ParkingLotSystemException e) {
-            Assert.assertEquals(ParkingLotSystemException.ExceptionType.WRONG_VEHICLE, e.type);
+            Assert.assertEquals(ParkingLotSystemException.ExceptionType.VEHICLE_ALREADY_UNPARKED_OR_WRONG_VEHICLE, e.type);
         }
     }
 
     @Test
-    public void givenAVehicle_WhenAlreadyParkedAndTriedToParkAgain_ShouldThrowException() {
+    public void givenManyVehicles_WhenParkingLotSizeIsFull_ShouldThrowException() {
         try {
-            parkingLotSystem.park(vehicle);
-            parkingLotSystem.park(vehicle);
+            parkingLotSystem.park("Tata Indigo CS");
+            parkingLotSystem.park("Toyota Fortuner");
+            parkingLotSystem.park("Maruti Swift Dzire");
+            parkingLotSystem.park("Tata Hexa");
+            parkingLotSystem.park("Maruti 800");
+            parkingLotSystem.park("Suzuki Nexa");
         } catch (ParkingLotSystemException e) {
             Assert.assertEquals(ParkingLotSystemException.ExceptionType.PARKING_LOT_FULL, e.type);
         }
@@ -51,10 +55,11 @@ public class ParkingLotSystemTest {
     @Test
     public void givenAVehicle_WhenAlreadyUnParkedAndTriedToUnParkAgain_ShouldThrowException() {
         try {
-            parkingLotSystem.unPark(vehicle);
-            parkingLotSystem.unPark(vehicle);
+            parkingLotSystem.park("Tata Indigo CS");
+            parkingLotSystem.unPark("Tata Indigo CS");
+            parkingLotSystem.unPark("Tata Indigo CS");
         } catch (ParkingLotSystemException e) {
-            Assert.assertEquals(ParkingLotSystemException.ExceptionType.VEHICLE_ALREADY_UNPARKED, e.type);
+            Assert.assertEquals(ParkingLotSystemException.ExceptionType.VEHICLE_ALREADY_UNPARKED_OR_WRONG_VEHICLE, e.type);
         }
     }
 }
