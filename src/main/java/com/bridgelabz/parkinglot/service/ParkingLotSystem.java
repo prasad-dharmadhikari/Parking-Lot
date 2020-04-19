acba0e666afab8d35d5b8c156e27b321186424fc
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-20 00:51:30 +0530

    REFACTORED : Delegated the parking job to parking attendant

--- src/main/java/com/bridgelabz/parkinglot/service/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/parkinglot/service/ParkingLotSystem.java
@@ -3,21 +3,26 @@ package com.bridgelabz.parkinglot.service;
 import com.bridgelabz.parkinglot.Observer.Observer;
 import com.bridgelabz.parkinglot.Observer.Subject;
 import com.bridgelabz.parkinglot.exception.ParkingLotSystemException;
+import com.bridgelabz.parkinglot.utility.ParkingAttendant;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ParkingLotSystem implements Subject {
 
-    private int parkingLotCapacity;
-    private String vehicleName;
-    private List<String> parkingLot;
+    public int parkingLotCapacity;
+    public String vehicleName;
+    public HashMap<Integer, String> parkingLot;
     private List<Observer> observers = new ArrayList<Observer>();
+    ParkingAttendant parkingAttendant = new ParkingAttendant(5);
 
     public ParkingLotSystem(int parkingLotCapacity) {
         this.parkingLotCapacity = parkingLotCapacity;
-        this.parkingLot = new ArrayList<String>();
+        this.parkingLot = new HashMap<>();
+        for (int itr = 1; itr <= parkingLotCapacity; itr++) {
+            parkingLot.put(itr, null);
+        }
     }
+
 
     @Override
     public void register(Observer o) {
@@ -32,27 +37,54 @@ public class ParkingLotSystem implements Subject {
     @Override
     public void notifyObservers() {
         for (Observer observer : observers) {
-            observer.sendParkingStatus(parkingLot.size(), parkingLotCapacity);
+            observer.sendParkingStatus(parkingLot);
         }
     }
 
 
     public void park(String vehicle) throws ParkingLotSystemException {
-        if (parkingLot.size() >= parkingLotCapacity)
-            throw new ParkingLotSystemException(ParkingLotSystemException.ExceptionType.PARKING_LOT_FULL, "PARKING LOT IS FULL");
         this.vehicleName = vehicle;
-        parkingLot.add(vehicleName);
+        parkingLot = parkingAttendant.park(vehicleName, parkingLot);
         this.notifyObservers();
     }
 
-    public void unPark(String vehicle) throws ParkingLotSystemException {
-        if (!(parkingLot.contains(vehicle)))
+    public boolean unPark(String vehicle) throws ParkingLotSystemException {
+        if (!isVehiclePresentInLot(vehicle))
             throw new ParkingLotSystemException(ParkingLotSystemException.ExceptionType.VEHICLE_ALREADY_UNPARKED_OR_WRONG_VEHICLE, "VEHICLE IS ALREADY UNPARKED");
-        parkingLot.remove(vehicle);
+        Iterator<String> parkingLotIterator = getParkingLotIterator(parkingLot);
+        this.vehicleName = vehicle;
+        while (parkingLotIterator.hasNext()) {
+            if (parkingLotIterator.next().equals(vehicle)) {
+                parkingLotIterator.remove();
+                return true;
+            }
+        }
         this.notifyObservers();
+        return true;
+    }
+
+    private boolean isVehiclePresentInLot(String vehicle) {
+        Iterator<String> parkingLotIterator = getParkingLotIterator(parkingLot);
+        while (parkingLotIterator.hasNext()) {
+            if (Objects.equals(parkingLotIterator.next(), vehicle))
+                return true;
+        }
+        return false;
+    }
+
+    private Iterator<String> getParkingLotIterator(HashMap<Integer, String> parkingLot) {
+        return parkingLot.values().iterator();
     }
 
     public boolean isVehicleParked() {
-        return parkingLot.contains(vehicleName);
+        return parkingLot.containsValue(vehicleName);
+    }
+
+    public static boolean isParkingLotFull(HashMap<Integer, String> parkingLot) {
+        for (int i = 1; i <= parkingLot.size(); i++) {
+            if (parkingLot.get(i) == null)
+                return false;
+        }
+        return true;
     }
 }
---
+++ src/main/java/com/bridgelabz/parkinglot/utility/ParkingAttendant.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.parkinglot.utility;
+
+public class ParkingAttendant {
+}
