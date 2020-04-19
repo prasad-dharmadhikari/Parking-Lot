feecd8c45666dbc630b0c87b3810d8cf48c4f647
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-20 00:52:19 +0530

    ADDED : ParkingAttendant class to park the vehicle

--- src/main/java/com/bridgelabz/parkinglot/utility/ParkingAttendant.java
+++ src/main/java/com/bridgelabz/parkinglot/utility/ParkingAttendant.java
@@ -1,4 +1,24 @@
 package com.bridgelabz.parkinglot.utility;
 
+import com.bridgelabz.parkinglot.Observer.Owner;
+import com.bridgelabz.parkinglot.exception.ParkingLotSystemException;
+import com.bridgelabz.parkinglot.service.ParkingLotSystem;
+
+import java.util.HashMap;
+
 public class ParkingAttendant {
+    Owner owner = new Owner();
+    int parkingLotCapacity = 0;
+
+    public ParkingAttendant(int parkingLotCapacity) {
+        this.parkingLotCapacity = parkingLotCapacity;
+    }
+
+    public HashMap<Integer, String> park(String vehicle, HashMap<Integer, String> parkingLot) throws ParkingLotSystemException {
+        Integer emptyParkingSlot = owner.getEmptyParkingSlot(parkingLot);
+        if (ParkingLotSystem.isParkingLotFull(parkingLot))
+            throw new ParkingLotSystemException(ParkingLotSystemException.ExceptionType.PARKING_LOT_FULL, "PARKING LOT IS FULL");
+        parkingLot.put(emptyParkingSlot, vehicle);
+        return parkingLot;
+    }
 }
